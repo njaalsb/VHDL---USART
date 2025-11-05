a578eac918a984f9afabaaccc4fc33c076416cd2
njaalsb <bruheimnjal@gmail.com>
2025-11-05 16:00:26 +0100

    Opprett baud_gen og gjorde endringer i readme

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # VHDL---USART
 
-### Endring blabla
+# Logg
+
+## 5/11/2025
+- Vi har startet på Rx-modulen og CTRL-modulen
+- Elise har lært Github
+- Gjort generell planlegging av videre arbeid 
+# Hva må gjøres til neste gang?
+- Teste opplastning av kode på DE10 
+- Teste 7-segment display
+- Bli ferdig med baud_gen og lage testbenk
--- baud_gen.vhd
+++ baud_gen.vhd
@@ -8,21 +8,31 @@ use ieee.numeric_std.all;
 
 entity baud_gen is 
     generic (
-        F_CLk : integer := 50_000_000;
+        F_CLK : integer := 5208; -- divider
     );
     port (
-        clk : in std_logic;
-        baud: in std_logic_vector(7 downto 0);
+        clk : in std_logic; -- clk inn 
+        rst : in std_logic; --rst
+        baud: in std_logic_vector(7 downto 0); -- ønsket baud som input
         baud_clk : out std_logic;
     );
 end entity baud_gen;
 
 architecture RTL of baud_gen is 
-    -- signals etc
+    -- signal
+    signal count0   : natural range 0 to F_CLK-1; --count er signal fordi den skal oppdatere seg en gang etter hver gjennomkjøring 
 begin
     p1: process(clk) is
-        
+        if rst = '1' then
+            count0 <= 0;
+        elsif rising_edge(clk) then
+            -- inkrementerer counter til divider når den er ulik divider
+            if count0 /= F_CLK-1 then
+                count0 <= count0 + 1;
+            else
+                count0 <= 0;
+                -- resten av logikken må skje her...
+            end if;
+        end if;
     end process p1;
-
-
 end architecture RTL;
