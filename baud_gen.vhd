c80179e9f9606f0fbcbe8d0b4c7f816f07543768
njaalsb <bruheimnjal@gmail.com>
2025-11-05 14:43:09 +0100

    oppdatert baud_gen

--- baud_gen.vhd
+++ baud_gen.vhd
@@ -1,3 +1,7 @@
+-- Baud generator til USART
+-- 8x oversampling incl (2x mtp aliasing)
+-- 9600 baud -> krever 153 600 sampling rate
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -9,5 +13,16 @@ entity baud_gen is
     port (
         clk : in std_logic;
         baud: in std_logic_vector(7 downto 0);
+        baud_clk : out std_logic;
     );
 end entity baud_gen;
+
+architecture RTL of baud_gen is 
+    -- signals etc
+begin
+    p1: process(clk) is
+        
+    end process p1;
+
+
+end architecture RTL;
