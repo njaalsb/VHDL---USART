2f721513a38c548ab08305c5bd1fea3660664885
elisemariehals <elisemariehals@gmail.com>
2025-11-05 14:41:10 +0100

    commit bla

--- testfil.vhd
+++ testfil.vhd
@@ -1,1 +1,9 @@
---hello
+--hello[
+    {
+        "type": "command",
+        "details": {
+            "key": "git config --global user.name",
+            "value": "Your Name"
+        }
+    }
+]
