6e6ae3f1d472252929d26350afbc46bf05f82e1c
elisemariehals <elisemariehals@gmail.com>
2025-11-09 22:13:36 +0100

    Oppdatert, ikke store endringer

--- uart_ctrl.vhd
+++ uart_ctrl.vhd
@@ -23,44 +23,40 @@ end entity uart_ctrl;
 
 architecture rtl of uart_ctrl is
 
-type state_type is (IDLE, LATCH_RX);                                 -- Tilstander, vent og latch mottatt data
-signal state: state_type := IDLE;                                    -- Nåværende tilstand
-signal led_cnt: integer range 0 to 2_000_000;                        -- Juster størrelsen etter ønsket puls lengde
-signal reg_rx: std_logic_vector(7 downto 0) := (others =>); '0';     -- Register for mottatt data
+    type state_type is (IDLE);                                             -- Tilstander, nå: vent
+    signal state: state_type := IDLE;                                    -- Nåværende tilstand
 
-process(clk, rstn)
-begin   
+    signal led_cnt: integer range 0 to 2_000_000;                        -- Juster størrelsen etter ønsket puls lengde
+    signal reg_rx: std_logic_vector(7 downto 0) := (others =>); '0';     -- Register for mottatt data
+begin
+
+ process(clk, rstn)
+    begin   
     if rstn = '0' then
         sevenseg <= (others => '0');
         led_pulse <= '0';
         baud_value <= (others => '0');
+        led_cnt <= 0;
+        reg_rx <= (others => '0');
         state <= IDLE;
+
     elsif rising_edge(clk) then
         case state is
             when IDLE =>
-             if rx_valid = '1' then
-                    reg_rx <= rx_data;
-                    sevenseg <= rx_data;
+                if rx_valid = '1' then
+                    reg_rx <= rx_data; --
+                    sevenseg <= rx_data;     --viser ASCII verdi direkte på 7-segment display
                     led_cnt <= '2_000_000';  -- Juster etter klokkehastighet for ønsket LED puls lengde
-                    state <= LATCH_RX;
-                else
-                    if led_cnt > 0 then
+
+                elsif led_cnt > 0 then
                         led_cnt <= led_cnt - 1;
                         led_pulse <= '1';
-                    else
-                    led_pulse <= '0';
+                else
+                    led_pulse <= '0';  
                 end if;
-            end if;
-
-            when LATCH_RX =>
-                led_pulse <= '0';
-                state <= IDLE;
-
-            when others =>
-                state <= IDLE;
         end case;
     end if;
-end process;
+ end process;
 
 end architecture rtl;
 
